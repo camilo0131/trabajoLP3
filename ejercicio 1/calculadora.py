4e466eb34ce8a6ff9352c79ad949030f37a7b0ed
camilo0131 <lopesgomescamilo0131@gmail.com>
2024-01-20 11:56:59 -0500

    Correcion ejercicio uno

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,21 @@
 def calculadora():
-    num1 = float("Ingrese el primer número: ")
-    num2 = float("Ingrese el segundo número: ")
-    operacion = input("Ingrese la operación (+, -, *, /): ")
+
+    num1 = float (input("Ingrese el primer número: "))
+    num2 = float (input("Ingrese el segundo número: "))
+    operacion = (input("Ingrese la operación (+, -, *, /): "))
 
     if operacion == '+':
-        resultado = num + num2
+        resultado = num1 + num2
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num / num2
+        resultado = num1 / num2
     else:
         resultado = "Operación no válida"
 
-    print("Resultado: "resultado)
+    return print(resultado)
 
-calculdora()
+calculadora()
+ 
