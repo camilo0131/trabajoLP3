e964c083573553a0ce3049eda9d0160cc416ce7d
camilo0131 <lopesgomescamilo0131@gmail.com>
2024-01-27 08:52:53 -0500

    Pull

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,21 +1,24 @@
-def calculadora():
 
+
+def calculadora():
+   
     num1 = float (input("Ingrese el primer número: "))
     num2 = float (input("Ingrese el segundo número: "))
     operacion = (input("Ingrese la operación (+, -, *, /): "))
 
     if operacion == '+':
-        resultado = num1 + num2
+        resultado = num1 + num2 
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num1 / num2
+        resultado = num1 / num2 
     else:
         resultado = "Operación no válida"
 
-    return print(resultado)
+    return print(resultado) 
 
-calculadora()
+calculadora() 
+
  
