25f77fd6c53e3eea81521bf18c5566c85a10d89f
camilo0131 <146980390+camilo0131@users.noreply.github.com>
2024-01-27 00:12:22 -0500

    adelanto ejercicio 8

--- ejercicio 8/contadorPalabras.py
+++ ejercicio 8/contadorPalabras.py
@@ -1,11 +1,12 @@
-def contar_palabras_en_archivo(nombre_archivo)
+def contar_palabras_en_archivo(nombre_archivo):#se agrega dos puntos(:)
     try:
         with open(nombre_archivo, 'r') as archivo:
             contenido = archivo.read()
             palabras = contenido.split()
-            return len(palabras)
-    except FileNotFoundError:
-        retunr f"El archivo {nombre_archivo} no fue encontrado."
+    except FileNotFoundError:    
+            return len(palabras)#se corrige la palabra return
+    except Exception as e:
+        return f"El archivo {nombre_archivo} no fue encontrado."
 
 archivo_nombre = input("Ingrese el nombre del archivo de texto: ")
 print(f"El archivo contiene {contar_palabras_en_archivo(archivo_nombre)} palabras.")
