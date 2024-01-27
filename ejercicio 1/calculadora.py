121d108a14da6f90659c182ca7bb0554badef35d
camilo0131 <146980390+camilo0131@users.noreply.github.com>
2024-01-26 22:09:29 -0500

    correccion ejercicio 1

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,21 +1,26 @@
+  #se define una funcion (calculadora)
+
 def calculadora():
 
-    num1 = float (input("Ingrese el primer número: "))
-    num2 = float (input("Ingrese el segundo número: "))
+  #se solicita agregar un numero, luego pasa a ser el valor de la variable
+
+    num1 = float (input("Ingrese el primer número: "))   #se agrega un input
+    num2 = float (input("Ingrese el segundo número: "))  #se agrega un input
+  #define el valor a la operacion a solicitar  
     operacion = (input("Ingrese la operación (+, -, *, /): "))
 
     if operacion == '+':
-        resultado = num1 + num2
+        resultado = num1 + num2  #se corrige variable num a num1
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num1 / num2
+        resultado = num1 / num2  #se corrige variable num a num1
     else:
         resultado = "Operación no válida"
+  # devuelve el resultado de la función
+    return print(resultado) #se agrega un return
 
-    return print(resultado)
-
-calculadora()
+calculadora() # se corrige palabra calculadora
  
