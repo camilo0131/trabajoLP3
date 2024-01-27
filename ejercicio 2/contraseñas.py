7197a729698da30c1a192f799f1d9ec551c9b21a
camilo0131 <146980390+camilo0131@users.noreply.github.com>
2024-01-26 22:36:19 -0500

    correccion ejercicio 2

--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,14 @@
-import rando
+#se importan los modulos random & string
+
+import random #se completa la palabra random
 import string
-
-generar_contraseña(longitud=8):
+#se define una funcion
+def  generar_contraseña(longitud=8):# se define generar_contraseña
+    #se crea una cadena de caracteres
     caracteres = string.ascii_letters + string.digits + string.punctuation
+    #se genera una contraseña
     contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
-    retunr contraseña
-
-print(generar_contrasena())
+    #se devuelve la contraseña generada
+    return  contraseña #se corrige la palabra return
+#se llama a la funcion para imprimir la contraseña
+print(generar_contraseña())#se corrige la palabra contraseña
