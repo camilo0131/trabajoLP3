0eed7ff9bb7a2b8360e2be43ce4ef8d6cd28c8b8
camilo0131 <146980390+camilo0131@users.noreply.github.com>
2024-01-27 00:52:25 -0500

    correccion ejercicio 10

--- ejercicio 10/descuento.py
+++ ejercicio 10/descuento.py
@@ -1,9 +1,17 @@
-calcular_precio_con_decuento(precio_base, porcentaje_descuento)
+#se define una funcion la cual toma dos parametros
+def calcular_precio_con_descuento(precio_base, porcentaje_descuento):#se define una funcion y se le agrega dos puntos (:)
+    #calcula el monto tomando el precio base
     descuento = precio_base * (porcentaje_descuento / 100)
+    #calcula el precio final restando el descuento
     precio_final = precio_base - descuento
-    retunr precio_final
+   #devuelve el precio final del producti
+    return precio_final# se corrige la palabra return
 
-precio_base = float("Ingrese el precio base del producto: ")
+#solicita ingresar el precio del producto
+precio_base = float(input("Ingrese el precio base del producto:"))#se agrega un input
+#se solicita ingresar el porcentaje de descuento
 porcentaje_descuento = float(input("Ingrese el porcentaje de descuento: "))
+#se llama a la funcion (calcular_precio_con_descuento) con los valores ingresados y guarda el resultado en la variable(precio final)
 precio_final = calcular_precio_con_descuento(precio_base, porcentaje_descuento)
+#imprime el precio final del producto
 print(f"El precio final con {porcentaje_descuento}% de descuento es: {precio_final}")
